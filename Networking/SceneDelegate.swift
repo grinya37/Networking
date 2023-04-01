77bc08800254bf4f7018e4dd0274a4dafa63694e
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-04-02 01:08:38 +0300

    add_files

---
+++ Networking/SceneDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  SceneDelegate.swift
+//  Networking
+//
+//  Created by Николай Гринько on 01.04.2023.
+//  Copyright © 2023 Alexey Efimov. All rights reserved.
+//
+
+//import Foundation
+//import UIKit
+//
+//@available(iOS 13.0, *)
+//class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+//
+//    var window: UIWindow?
+//
+//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        guard let _ = (scene as? UIWindowScene) else { return }
+//        guard let windowScene = scene as? UIWindowScene else { return }
+//        window = UIWindow(windowScene: windowScene)
+//        //let tabBarController = TabBarController()
+//       // window?.rootViewController = tabBarController
+//        window?.makeKeyAndVisible()
+//    }
+//
+//    func sceneDidDisconnect(_ scene: UIScene) {
+//    }
+//
+//    func sceneDidBecomeActive(_ scene: UIScene) {
+//    }
+//
+//    func sceneWillResignActive(_ scene: UIScene) {
+//    }
+//
+//    func sceneWillEnterForeground(_ scene: UIScene) {
+//    }
+//
+//    func sceneDidEnterBackground(_ scene: UIScene) {
+//
+//    }
+//}
+//
